aed128afb2c37b6bf40de7e857f95a31d3609894
Ednilson Alves Ribeiro <130374794+ribeiroednilson1234@users.noreply.github.com>
2026-09-07 09:22:27 -0400

    Update concursadoAprovado class with new properties
    Refactor concursadoAprovado class to include Concursado.*_file property and update constructor and methods accordingly.

--- Cadastrado.ts
+++ Cadastrado.ts
@@ -1,18 +1,20 @@
-TypeScript {
-
+Json: {
+"[TypeScript]": "export interfaceUIEstudo_DB.Sql",
+  }; {
 class concursadoAprovado {
   private name: char;
   private idade: int;
   private identidade: varchar;
-  private CPF: int;	
+  private CPF: int;
+  private Concursado.*_file;
 
-  public constructor(Id_name: char, Id_idade: int, Id_identidade: varchar, Id_CPF: int) {
+  public constructor(Id_name: char, Id_idade: int, Id_identidade: varchar, Id_CPF: int, Concursado.*_file) {
     this.Name == Name;
     this.Idade == Idade;
     this.Identidade == Identidade;
     this.CPF == CPF;
+    this.Concursado.*_file == Concursado.*_file;
   };
-
   public getName(): char {
     return this.Name;
   };
@@ -25,8 +27,9 @@ class concursadoAprovado {
   public getCPF(): int {
     return this.CPF;
   };
+  public getConcursado.*_file(): file {
+    return this.Concursado.*_file;
+  };
 };
-      
-const Estudo_DB.Sql = new Cadastro("*", "*", "*", "*");
-
-console.log(Estudo.getconcursadoArpovado()); // Estudo.Cadastrado isn't accessible from outside the class since it's private}
+const Estudo_DB.Sql = new concursadoContratado('*', '*', '*', '*', '*');
+console.log(Estudo_DB.Sql.getconcursadoArpovado()); // Estudo.Cadastrado isn't accessible from outside the class since it's private}
