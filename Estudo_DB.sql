2b2af578672e8d7461b5e15ea66577962f7cf68f
Ednilson Alves Ribeiro <130374794+ribeiroednilson1234@users.noreply.github.com>
2026-09-07 08:49:49 -0400

    Refactor SQL script for database and tables

--- Estudo_DB.sql
+++ Estudo_DB.sql
@@ -1,14 +1,14 @@
-CREATE DATABASE ESCHEMAS ESTUDO; GO
+CREATE DATABASE ESCHEMAS ESTUDO_DB.Sql; GO
 if CONTRATADO{
-CREATE TABLE CONTRATADO( PRIMARY KEY id_CPF int(11)[not null],
+CREATE TABLE CONTRATADO( PRIMARY KEY Id_CPF int(11)[not null],
 Idx_Nome_char(50) [not null],
 Idade_int(3) [not null],
-Identidade_varchar (11) [not null],
+Idx_Identidade_varchar (11) [not null],
 Idx_CPF_int(11) [not null],
-CREATE INDEX idx_CPF ON CONTRATADO (CPF);
+CREATE INDEX Idx_CPF ON CONTRATADO (CPF);
 )ENGINE=InnoDB;
 
-INSERT INTO CONTRATADO[(id_Identidade,Nome,Idade,CPF)
+INSERT INTO CONTRATADO[(Id_Identidade,Nome,Idade,Id_CPF)
 VALUES('','','','');
 GO
 };
@@ -16,23 +16,25 @@ if CONTRATADO {
 CREATE TABLE CADASTRO( FORYGNE KEY id_Nome varchar(18)[not null],
 Idx_Nome_char(50) [not null],
 Idade_int(3) [not null],
-Identidade_varchar(18) [not null],
-CPF_int(11) [not null],
+Idx_Identidade_varchar(18) [not null],
+Idx_CPF_int(11) [not null],
 Idx_Concursado.*_file() [not null],
 CREATE INDEX Idx_Concursado.*, function(Concursado.*) ON CADASTRO (Concursado.*_file);
 )ENGINE=MongoDB;
 
-INSERT INTO CADASTRO[(id_Identidade,Nome,Idade,CPF,Concursado.*)
+INSERT INTO CADASTRO[(id_Identidade,id_Nome,Idade,id_CPF,Concursado.*_file)
 VALUES('','','','','');
 GO
 };
 else {
 ALTER TABLE CADASTRO ADD/DROP(
-id_Identidade_varchar(18)[not null]
-Nome_char(50) [not null],
+Idx_Identidade_varchar(18)[not null]
+Idx_Nome_char(50) [not null],
 Idade_int(3) [not null],
-CPF_int(11) [not null],
+Idx_CPF_int(11) [not null],
+  Concursado.*_file() [not null],
 CREATE INDEX idx_CPF ON CONTRATADO (CPF);
 )ENGINE=InnoDB;
 GO
 SELECT DISTINCT Nome, Identidade, CPF FROM CADASTRO WHERE ESTUDO ORDER BY asc A-Z, a-Z, a-z, Az;
+GO
