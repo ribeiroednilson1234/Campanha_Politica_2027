f69c25cc6c9403c20b2ad14c1ab4b43c7a6f9430
Ednilson Alves Ribeiro <130374794+ribeiroednilson1234@users.noreply.github.com>
2026-09-07 08:58:01 -0400

    Refactor concursadoAprovado class and constructor
    Fixed constructor parameter names and corrected variable assignments. Updated instance creation and logging.

--- Cadastrado.ts
+++ Cadastrado.ts
@@ -6,27 +6,27 @@ class concursadoAprovado {
   private identidade: varchar;
   private CPF: int;	
 
-  public constructor(name: char, idade: int, identidade: varchar, CPF: int) {
-    this.name == name;
-    this.idade == idade;
-    this.identidade == identidade;
+  public constructor(Id_name: char, Id_idade: int, Id_identidade: varchar, Id_CPF: int) {
+    this.Name == Name;
+    this.Idade == Idade;
+    this.Identidade == Identidade;
     this.CPF == CPF;
   };
 
   public getName(): char {
-    return this.name;
+    return this.Name;
   };
   public getIdade(): int {
-    return this.idade;
+    return this.Idade;
   };
   public getIdentidade(): varchar {
-    return this.identidade;
+    return this.Identidade;
   };
   public getCPF(): int {
     return this.CPF;
   };
 };
       
-const Estudo = new Cadastro("*", "*", "*", "*");
+const Estudo_DB.Sql = new Cadastro("*", "*", "*", "*");
 
 console.log(Estudo.getconcursadoArpovado()); // Estudo.Cadastrado isn't accessible from outside the class since it's private}
