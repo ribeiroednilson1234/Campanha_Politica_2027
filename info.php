0643cc81b824b691b3255c5b08d6aa387d268125
Ednilson Alves Ribeiro <130374794+ribeiroednilson1234@users.noreply.github.com>
2026-09-11 11:14:38 -0400

    Add files via upload

---
+++ info.php
@@ -0,0 +1,17 @@
+<?php 
+php phpinfo();
+memory_limit = 256M;
+upload_max_filesize = 32M;
+post_max_size = 32M;
+max_execution_time = 300";
+cgi.force_redrect = .$_SERVER['DOCUMENT_ROOT'] dirmane(['home/ovni/Campanha/enviar']);
+doc_root string = [.home/ovni/Campanha/enviar;Campanha.com]
+auto_global_jit bool;
+enable_post_data_reading bool;
+auto_prepend_file string;
+default_charset string;
+include_path=.:/php/includes";
+include_path= ".:${USER}.{LOGIN}/pear/php";
+file_uploads bool;
+sql.safe_mode = '1';
+?>
