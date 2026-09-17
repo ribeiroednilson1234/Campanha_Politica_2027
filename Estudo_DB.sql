be680acbe2c6a9f75b516b81aff461bf883a6603
Ednilson Alves Ribeiro <130374794+ribeiroednilson1234@users.noreply.github.com>
2026-09-17 16:02:05 -0400

    Add files via upload

---
+++ Estudo_DB.sql
@@ -0,0 +1,39 @@
+*.SQL
+CREATE DATABASE ESCHEMAS ESTUDO; GO
+if CONTRATADO{
+CREATE TABLE CONTRATADO( PRIMARY KEY id_CPF int(11)[not null],
+Idx_Nome_char(50) [not null],
+Idade_int(3) [not null],
+Identidade_varchar (11) [not null],
+Idx_CPF_int(11) [not null],
+CREATE INDEX idx_CPF ON CONTRATADO (CPF);
+)ENGINE=InnoDB;
+
+INSERT INTO CONTRATADO[(id_Identidade,Nome,Idade,id_CPF)
+VALUES('','','','');
+GO
+};
+if CONTRATADO {
+CREATE TABLE CADASTRO( FORYGNE KEY id_Nome varchar(18)[not null],
+Idx_Nome_char(50) [not null],
+Idade_int(3) [not null],
+Identidade_varchar(18) [not null],
+CPF_int(11) [not null],
+Idx_Concursado*() [not null],
+CREATE INDEX Idx_Concursado.*, function(Concursado.*) ON CADASTRO (Concursado.*_file);
+)ENGINE=MongoDB;
+
+INSERT INTO CADASTRO[(id_Identidade,Nome,Idade,id_CPF,Concursado.*)
+VALUES('','','','','');
+GO
+};
+else {
+ALTER TABLE CADASTRO ADD/DROP(
+id_Identidade_varchar(18)[not null]
+Nome_char(50) [not null],
+Idade_int(3) [not null],
+CPF_int(11) [not null],
+CREATE INDEX idx_CPF ON CONTRATADO (CPF);
+)ENGINE=InnoDB;
+GO
+SELECT DISTINCT Nome, Identidade, CPF, Concursado.*_file FROM CADASTRO WHERE ESTUDO ORDER BY asc A-Z, a-Z, a-z, Az;
